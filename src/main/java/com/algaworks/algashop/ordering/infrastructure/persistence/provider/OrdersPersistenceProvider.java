8fba438e71208b04fc0ac9db9a249d3f46a5abcf
thiag <thiagoaataide@gmail.com>
2026-03-16 07:50:52 -0300

    - Aplicando um Persistence Provider para separar a camada de domínio da camada de persistência.

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
@@ -0,0 +1,43 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.provider;
+
+import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.repository.Orders;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import com.algaworks.algashop.ordering.infrastructure.persistence.repository.OrdersPersistenceEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class OrdersPersistenceProvider implements Orders {
+
+    private final OrdersPersistenceEntityRepository persistenceRepository;
+
+    @Override
+    public Optional<Order> ofId(OrderId orderId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean exists(OrderId orderId) {
+        return false;
+    }
+
+    @Override
+    public void add(Order aggregateRoot) {
+        var persistenceEntity = OrderPersistenceEntity.builder()
+                .id(aggregateRoot.id().value().toLong())
+                .customerId(aggregateRoot.customerId().value())
+                .build();
+
+        persistenceRepository.saveAndFlush(persistenceEntity);
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+}
