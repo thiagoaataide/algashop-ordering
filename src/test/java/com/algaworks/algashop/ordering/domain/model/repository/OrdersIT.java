7c351c621369ffff25f0007e429b072c07f8cdc4
thiagoaataide <thiagoaataide@gmail.com>
2026-03-17 13:21:19 -0300

    - Garantindo o estado da entidade de persistência e de domínio ao inserir ou atualizar.

--- src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
@@ -1,11 +1,13 @@
 package com.algaworks.algashop.ordering.domain.model.repository;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.entity.OrderStatus;
 import com.algaworks.algashop.ordering.domain.model.entity.OrderTestDataBuilder;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
 import com.algaworks.algashop.ordering.infrastructure.persistence.assembler.OrderPersistenceEntityAssembler;
 import com.algaworks.algashop.ordering.infrastructure.persistence.disassembler.OrderPersistenceEntityDisassembler;
 import com.algaworks.algashop.ordering.infrastructure.persistence.provider.OrdersPersistenceProvider;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -49,5 +51,18 @@ class OrdersIT {
                 s -> assertThat(s.status()).isEqualTo(originalOrder.status()),
                 s -> assertThat(s.paymentMethod()).isEqualTo(originalOrder.paymentMethod())
          );
+    }
+
+    @Test
+    public void shouldUpdateExistingOrder(){
+        Order order = OrderTestDataBuilder.anOrder().status(OrderStatus.PLACED).build();
+        orders.add(order);
+
+        order = orders.ofId(order.id()).orElseThrow();
+        order.markAsPaid();
+        orders.add(order);
+        order = orders.ofId(order.id()).orElseThrow();
+
+        Assertions.assertThat(order.isPaid()).isTrue();
     }
 }
---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProviderIT.java
@@ -0,0 +1,58 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.provider;
+
+import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.entity.OrderStatus;
+import com.algaworks.algashop.ordering.domain.model.entity.OrderTestDataBuilder;
+import com.algaworks.algashop.ordering.infrastructure.persistence.assembler.OrderPersistenceEntityAssembler;
+import com.algaworks.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
+import com.algaworks.algashop.ordering.infrastructure.persistence.disassembler.OrderPersistenceEntityDisassembler;
+import com.algaworks.algashop.ordering.infrastructure.persistence.repository.OrdersPersistenceEntityRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@Import({
+        OrdersPersistenceProvider.class,
+        OrderPersistenceEntityAssembler.class,
+        OrderPersistenceEntityDisassembler.class,
+        SpringDataAuditingConfig.class
+})
+class OrdersPersistenceProviderIT {
+    private OrdersPersistenceProvider persistenceProvider;
+    private OrdersPersistenceEntityRepository entityRepository;
+
+    @Autowired
+    public OrdersPersistenceProviderIT(OrdersPersistenceProvider persistenceProvider, OrdersPersistenceEntityRepository entityRepository) {
+        this.persistenceProvider = persistenceProvider;
+        this.entityRepository = entityRepository;
+    }
+
+    @Test
+    public void shouldUpdateAndKeepPersistenceEntityState() {
+        Order order = OrderTestDataBuilder.anOrder().status(OrderStatus.PLACED).build();
+        long orderId = order.id().value().toLong();
+        persistenceProvider.add(order);
+
+        var persistenteEntity = entityRepository.findById(orderId).orElseThrow();
+        Assertions.assertThat(persistenteEntity.getStatus()).isEqualTo(OrderStatus.PLACED.name());
+
+        Assertions.assertThat(persistenteEntity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(persistenteEntity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(persistenteEntity.getLastModifiedByUserId()).isNotNull();
+
+        order = persistenceProvider.ofId(order.id()).orElseThrow();
+        order.markAsPaid();
+        persistenceProvider.add(order);
+
+        persistenteEntity = entityRepository.findById(orderId).orElseThrow();
+
+        Assertions.assertThat(persistenteEntity.getStatus()).isEqualTo(OrderStatus.PAID.name());
+
+        Assertions.assertThat(persistenteEntity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(persistenteEntity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(persistenteEntity.getLastModifiedByUserId()).isNotNull();
+    }
+}
