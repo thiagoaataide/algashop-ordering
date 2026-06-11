c1ba4bae0b02d99108b5e534c176bdfeeb8e3931
thiagoaataide <thiagoaataide@gmail.com>
2026-06-11 14:03:49 -0300

    Implementação do CheckoutService

--- src/main/java/com/algaworks/algashop/ordering/domain/model/service/CheckoutService.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/CheckoutService.java
@@ -4,10 +4,12 @@ import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.entity.PaymentMethod;
 import com.algaworks.algashop.ordering.domain.model.entity.ShoppingCart;
 import com.algaworks.algashop.ordering.domain.model.exception.ShoppingCartCantProceedToCheckoutException;
+import com.algaworks.algashop.ordering.domain.model.utility.DomainService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Billing;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Product;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Shipping;
 
+@DomainService
 public class CheckoutService {
 
     public Order checkout(ShoppingCart shoppingCart,
