226c68b3df09e6313870e30e1f7eca68aad3f29d
thiag <thiagoaataide@gmail.com>
2026-07-02 05:04:39 -0300

    Implementação Casos de Uso: - CheckoutApplicationService

---
+++ src/main/java/com/algaworks/algashop/ordering/aplication/checkout/CheckoutApplicationService.java
@@ -0,0 +1,51 @@
+package com.algaworks.algashop.ordering.aplication.checkout;
+
+import com.algaworks.algashop.ordering.domain.model.commons.ZipCode;
+import com.algaworks.algashop.ordering.domain.model.order.*;
+import com.algaworks.algashop.ordering.domain.model.order.shipping.OriginAddressService;
+import com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService;
+import com.algaworks.algashop.ordering.domain.model.shoppingcart.ShoppingCart;
+import com.algaworks.algashop.ordering.domain.model.shoppingcart.ShoppingCartId;
+import com.algaworks.algashop.ordering.domain.model.shoppingcart.ShoppingCartNotFoundException;
+import com.algaworks.algashop.ordering.domain.model.shoppingcart.ShoppingCarts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class CheckoutApplicationService {
+
+    private final ShoppingCarts shoppingCarts;
+    private final ShippingCostService shippingCostService;
+    private final OriginAddressService originAddressService;
+    private final CheckoutService checkoutService;
+    private final BillingInputDisassembler billingInputDisassembler;
+    private final ShippingInputDisassembler shippingInputDisassembler;
+    private final Orders orders;
+
+    @Transactional
+    public String checkout(CheckoutInput input){
+        Objects.requireNonNull(input);
+        PaymentMethod paymentMethod = PaymentMethod.valueOf(input.getPaymentMethod());
+
+        ShoppingCart shoppingCart = shoppingCarts.ofId(new ShoppingCartId(input.getShoppingCartId())).orElseThrow(() -> new ShoppingCartNotFoundException());
+
+        Billing billing = billingInputDisassembler.toDomainModel(input.getBilling());
+
+        ShippingCostService.CalculationResult calculationResult = shippingCostService.calculate(new ShippingCostService.CalculationRequest(
+                originAddressService.originAddress().zipCode(),
+                new ZipCode(input.getShipping().getAddress().getZipCode())
+        ));
+        Shipping shipping = shippingInputDisassembler.toDomainModel(input.getShipping(), calculationResult);
+
+        Order order = checkoutService.checkout(shoppingCart, billing, shipping, paymentMethod);
+        orders.add(order);
+
+        shoppingCarts.add(shoppingCart);
+
+        return order.id().toString();
+    }
+}
