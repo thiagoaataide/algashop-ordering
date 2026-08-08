c5b49efbb0b420dadf8a49016e75d034fc064a3a
thiag <thiagoaataide@gmail.com>
2026-08-08 11:42:00 -0300

    Simplificação da implementação

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityRepository.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityRepository.java
@@ -6,8 +6,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerPersistenceEntityRepository
-        extends JpaRepository<CustomerPersistenceEntity, UUID>,
-        CustomerPersistenceEntityQueries
+        extends JpaRepository<CustomerPersistenceEntity, UUID>
 {
     Optional<CustomerPersistenceEntity> findByEmail(String value);
 
