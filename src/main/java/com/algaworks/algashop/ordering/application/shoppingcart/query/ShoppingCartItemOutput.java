aa0c9075f52b47bebeff0c5f139049d2a3690007
thiagoaataide <thiagoaataide@gmail.com>
2026-08-20 13:12:03 -0300

    finalizado a implementação co CQRS

---
+++ src/main/java/com/algaworks/algashop/ordering/application/shoppingcart/query/ShoppingCartItemOutput.java
@@ -0,0 +1,17 @@
+package com.algaworks.algashop.ordering.application.shoppingcart.query;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class ShoppingCartItemOutput {
+  private UUID id;
+  private UUID productId;
+  private String name;
+  private BigDecimal price;
+  private Integer quantity;
+  private BigDecimal totalAmount;
+  private Boolean available;
+}
---
+++ src/main/java/com/algaworks/algashop/ordering/application/shoppingcart/query/ShoppingCartQueryService.java
@@ -0,0 +1,9 @@
+package com.algaworks.algashop.ordering.application.shoppingcart.query;
+
+import java.util.UUID;
+
+public interface ShoppingCartQueryService {
+    ShoppingCartOutput findById(UUID shoppingCartId);
+
+    ShoppingCartOutput findByCustomerId(UUID customerId);
+}
