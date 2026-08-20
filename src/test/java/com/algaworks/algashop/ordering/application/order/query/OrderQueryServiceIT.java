00b1e02a6f089d4d05732629bcce39429a43c5fc
thiagoaataide <thiagoaataide@gmail.com>
2026-08-20 13:00:07 -0300

    remoção de imports não utilizados

--- src/test/java/com/algaworks/algashop/ordering/application/order/query/OrderQueryServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/order/query/OrderQueryServiceIT.java
@@ -1,6 +1,5 @@
 package com.algaworks.algashop.ordering.application.order.query;
 
-import com.algaworks.algashop.ordering.application.utility.PageFilter;
 import com.algaworks.algashop.ordering.domain.model.customer.Customer;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerTestDataBuilder;
