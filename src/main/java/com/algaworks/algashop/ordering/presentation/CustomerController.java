ba6b0a5c64282ca528c19de68690e2100ed9c277
thiagoaataide <thiagoaataide@gmail.com>
2026-09-24 14:18:04 -0300

    - implementação para testes com retorno de tratamento de informações de erros. - Aplicado Mocks nos testes de contrato.

--- src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -2,7 +2,11 @@ package com.algaworks.algashop.ordering.presentation;
 
 import com.algaworks.algashop.ordering.application.commons.AddressData;
 import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,26 +15,16 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerOutput create(@RequestBody CustomerInput input) {
-        AddressData address = input.getAddress();
-
-        return CustomerOutput.builder()
-                .id(UUID.randomUUID())
-                .phone(input.getPhone())
-                .email(input.getEmail())
-                .firstName(input.getFirstName())
-                .lastName(input.getLastName())
-                .document(input.getDocument())
-                .birthDate(input.getBirthDate())
-                .registeredAt(OffsetDateTime.now())
-                .archived(false)
-                .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
-                .loyaltyPoints(0)
-                .address(address)
-                .build();
+    public CustomerOutput create(@RequestBody @Valid CustomerInput input) {
+        UUID customerId = customerManagementApplicationService.create(input);
+        return customerQueryService.findById(customerId);
     }
 }
--- src/test/java/com/algaworks/algashop/ordering/presentation/CustomerControllerContractTest.java
+++ src/test/java/com/algaworks/algashop/ordering/presentation/CustomerControllerContractTest.java
@@ -1,24 +1,38 @@
 package com.algaworks.algashop.ordering.presentation;
 
+import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerOutputTestDataBuilder;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @WebMvcTest(controllers = CustomerController.class)
 class CustomerControllerContractTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @MockitoBean
+    private CustomerManagementApplicationService customerManagementApplicationService;
+
+    @MockitoBean
+    private CustomerQueryService customerQueryService;
 
     @BeforeEach
     public void setupAll() {
@@ -33,6 +47,15 @@ class CustomerControllerContractTest {
 
     @Test
     public void createCustomerContract() {
+
+        CustomerOutput customerOutput = CustomerOutputTestDataBuilder.existing().build();
+
+        Mockito.when(customerManagementApplicationService.create(Mockito.any(CustomerInput.class)))
+                .thenReturn(UUID.randomUUID());
+
+        Mockito.when(customerQueryService.findById(Mockito.any(UUID.class)))
+                .thenReturn(customerOutput);
+
         String jsonInput = """
                 {
                   "firstName": "John",
@@ -85,6 +108,50 @@ class CustomerControllerContractTest {
                             "address.number", Matchers.is("2000"),
                             "address.complement", Matchers.is("apt 122")
                     );
+    }
+
+    @Test
+    public void createCustomerErrorContract(){
+        String jsonInput = """
+                {
+                  "firstName": "",
+                  "lastName": "",
+                  "email": "john.doe@gmail.com",
+                  "document": "12345",
+                  "phone": "31991953046",
+                  "birthDate": "1989-07-25",
+                  "promotionNotificationsAllowed": false,
+                  "address": {
+                    "street": "Bourbon Street",
+                    "number": "2000",
+                    "complement": "apt 122",
+                    "neighborhood": "North Ville",
+                    "city": "Yostfort",
+                    "state": "South Carolina",
+                    "zipCode": "12321"
+                  }
+                }
+                """;
+
+        RestAssuredMockMvc.given().
+                accept(MediaType.APPLICATION_JSON).
+                contentType(MediaType.APPLICATION_JSON).
+                body(jsonInput).
+                when()
+                        .post("/api/v1/customers").
+                then()
+                        .assertThat()
+                        .contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body(
+                        "status", Matchers.is(HttpStatus.BAD_REQUEST.value()),
+                        "type", Matchers.is("/errors/invalid-fields"),
+                        "title", Matchers.notNullValue(),
+                        "detail", Matchers.notNullValue(),
+                        "instance", Matchers.notNullValue(),
+                        "fields", Matchers.notNullValue()
+                );
+
     }
 
 
