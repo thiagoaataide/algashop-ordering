6e7a673474ec18f6074bcd984253092c681265ac
thiag <thiagoaataide@gmail.com>
2026-09-26 10:10:29 -0300

    - Correção de nome de campos nos components de customers. - Implmentação da validação de contrato usando o RestAssureMockMvc

--- src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -1,6 +1,5 @@
 package com.algaworks.algashop.ordering.presentation;
 
-import com.algaworks.algashop.ordering.application.commons.AddressData;
 import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
 import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
@@ -10,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @RestController
